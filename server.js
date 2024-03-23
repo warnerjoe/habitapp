3228e452302554f76b4abfd55e8c4a309f5d4299
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-03-22 23:20:59 -0500

    Added ability to serve index.html

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const port = 5000;
+const path = require('path');
+
+const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
